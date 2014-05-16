d83e6415c45e7bdfc1aa0dd6b6276d3c0e045ed9
Pym <github@eggiz.org>
2014-05-16 15:17:15 +0200

    Mod: remove useless DBAL connection injection

--- src/Pym/Provider/TableEmbedServiceProvider.php
+++ src/Pym/Provider/TableEmbedServiceProvider.php
@@ -11,7 +11,7 @@ class TableEmbedServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
         $app['table.embed'] = $app->protect(function (Table $table) use ($app) {
-            return new TableEmbed($app['db'], $table);
+            return new TableEmbed($table);
         });
     }
 
